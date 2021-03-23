6c5af300d6631885a3bc9f8d59eb3c107272800e
Ping <4ivrik@ukr.net>
2021-03-23 12:20:58 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Button } from './Button';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +15,96 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    isStartButtonVisible: true,
+    goods: goodsFromServer,
+  }
+
+  showList = () => {
+    this.setState(prevState => ({
+      isStartButtonVisible: !prevState.isStartButtonVisible,
+      goods: prevState.goods,
+    }));
+  }
+
+  reverseList = () => {
+    this.setState(prevState => ({
+      goods: [...prevState.goods].reverse(),
+    }));
+  }
+
+  sortAlphabetically = () => {
+    this.setState(prevState => ({
+      goods: [...prevState.goods].sort(
+        (current, next) => current.localeCompare(next),
+      ),
+    }));
+  }
+
+  sortByLength = () => {
+    this.setState(prevState => ({
+      goods: [...prevState.goods].sort(
+        (current, next) => next.length - current.length,
+      ),
+    }));
+  }
+
+  resetList = () => {
+    this.setState(() => ({
+      goods: goodsFromServer,
+    }));
+  }
+
+  render() {
+    const { isStartButtonVisible, goods } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Goods</h1>
+        {isStartButtonVisible && (
+          <Button
+            callback={this.showList}
+            text="Start"
+          />
+        )
+        }
+
+        {!isStartButtonVisible && (
+          <>
+            <ul>
+              {goods.map(product => (
+                <li key={product}>
+                  {product}
+                </li>
+              ))}
+            </ul>
+
+            <Button
+              callback={this.reverseList}
+              text="Reverse"
+            />
+
+            <Button
+              callback={this.sortAlphabetically}
+              text="Sort Alphabetically"
+            />
+
+            <Button
+              callback={this.sortByLength}
+              text="Sort By Length"
+            />
+
+            <Button
+              callback={this.resetList}
+              text="Reset"
+            />
+          </>
+        )
+      }
+      </div>
+    );
+  }
+}
 
 export default App;
